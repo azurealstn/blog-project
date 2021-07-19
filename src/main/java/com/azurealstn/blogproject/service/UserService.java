69240dc584ae44c37f944ae43d3826d3e12d0a1d
azurealstn <azurealstn@naver.com>
2021-07-20 00:34:53 +0900

    회원가입 로직 구현

---
+++ src/main/java/com/azurealstn/blogproject/service/UserService.java
@@ -0,0 +1,19 @@
+package com.azurealstn.blogproject.service;
+
+import com.azurealstn.blogproject.domain.user.User;
+import com.azurealstn.blogproject.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public Long save(User user) {
+        return userRepository.save(user).getId();
+    }
+}
--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -2,13 +2,31 @@
 
 let index = {
     init: function() {
-        $("#btn-save").on("click", () => {
+        $("#btn-save").on("click", () => { //this를 바인딩하기 위해 화샬표 함수 사용
             this.save();
         });
     },
 
     save: function() {
-        console.log("안녕");
+        let data = { //JavaScript Object
+            username: $("#username").val(),
+            password: $("#password").val(),
+            email: $("#email").val(),
+            nickname: $("#nickname").val()
+        }
+
+        $.ajax({
+            type: "POST", //Http method
+            url: "/api/v1/user", //API 주소
+            data: JSON.stringify(data), //JSON으로 변환
+            contentType: "application/json; charset=utf-8", //MIME 타입
+            dataType: "json" //응답 데이터
+        }).done(function(res) {
+            alert("회원가입이 완료되었습니다.");
+            location.href = "/";
+        }).fail(function(err) {
+            alert(JSON.stringify(err));
+        });
     }
 }
 index.init();
