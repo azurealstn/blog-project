058da9a1c04587f3d554e3c0c10adad8395b8f3a
azurealstn <azurealstn@naver.com>
2021-07-26 14:14:25 +0900

    수정

--- src/test/java/com/azurealstn/blogproject/BlogProjectApplicationTests.java
+++ src/test/java/com/azurealstn/blogproject/BlogProjectApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BlogProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
